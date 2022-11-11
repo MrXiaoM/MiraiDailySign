2d3e074ce2b3b16b7920d343f965b8ef2507ae84
ヒューマンワーキング <coolxiaom95@gmail.com>
2022-11-11 13:18:27 +0800

    add config and data using logic

--- src/main/kotlin/MiraiDailySign.kt
+++ src/main/kotlin/MiraiDailySign.kt
@@ -2,7 +2,11 @@ package top.mrxiaom.mirai.dailysign
 
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription
 import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
+import net.mamoe.mirai.contact.Group
 import net.mamoe.mirai.utils.info
+import top.mrxiaom.mirai.dailysign.config.DailySignConfig
+import top.mrxiaom.mirai.dailysign.data.SignUser
+import java.io.File
 
 object MiraiDailySign : KotlinPlugin(
     JvmPluginDescription(
@@ -15,7 +19,32 @@ object MiraiDailySign : KotlinPlugin(
         dependsOn("xyz.cssxsh.mirai.plugin.mirai-economy-core")
     }
 ) {
+    val loadedUsers = mutableMapOf<Long, SignUser>()
+    val loadedConfigs = mutableMapOf<Long, DailySignConfig>()
+    val defaultConfig by lazy { DailySignConfig() }
     override fun onEnable() {
+
+        defaultConfig.reload()
+
         logger.info { "Plugin loaded" }
     }
+
+    fun getUser(id: Long): SignUser {
+        return loadedUsers[id] ?: SignUser(id).also { loadedUsers[id] = it }
+    }
+
+    fun reloadConfig() {
+        defaultConfig.reload()
+        val groups = File(dataFolder, "groups").listFiles()?.mapNotNull {
+            it.nameWithoutExtension.toLongOrNull()
+        } ?: listOf()
+        loadedConfigs.keys.filter { !groups.contains(it) }.forEach(loadedConfigs::remove)
+        for (group in groups) {
+            val config = loadedConfigs[group] ?: DailySignConfig(group.toString()).also { loadedConfigs[group] = it }
+            config.reload()
+            config.loadRewards()
+        }
+    }
 }
+
+fun Group.getDailySign(): DailySignConfig = MiraiDailySign.loadedConfigs[id] ?: MiraiDailySign.defaultConfig
--- src/main/kotlin/config/DailySignConfig.kt
+++ src/main/kotlin/config/DailySignConfig.kt
@@ -1,13 +1,16 @@
 package top.mrxiaom.mirai.dailysign.config
 
-import net.mamoe.mirai.console.data.AutoSavePluginConfig
-import net.mamoe.mirai.console.data.ValueDescription
-import net.mamoe.mirai.console.data.ValueName
-import net.mamoe.mirai.console.data.value
+import net.mamoe.mirai.console.data.*
+import net.mamoe.mirai.contact.Group
+import net.mamoe.mirai.contact.User
+import xyz.cssxsh.mirai.economy.EconomyService
+import xyz.cssxsh.mirai.economy.economy
+import xyz.cssxsh.mirai.economy.globalEconomy
+import xyz.cssxsh.mirai.economy.service.EconomyCurrency
 
 class DailySignConfig(
     val groupId: String = "default"
-) : AutoSavePluginConfig("groups/$groupId") {
+) : ReadOnlyPluginConfig("groups/$groupId") {
     @ValueName("at")
     @ValueDescription("需要 @ 机器人才能签到")
     val at by value(true)
@@ -29,4 +32,36 @@ class DailySignConfig(
             "奖励金钱到群聊上下文 group:货币种类:数量\n" +
             "奖励金钱到全局上下文 global:货币种类:数量")
     val rewards by value(listOf("group:mirai-coin:100"))
+    val realRewards = mutableListOf<Reward>()
+    class Reward(
+        val isGlobal: Boolean,
+        val currency: EconomyCurrency,
+        val money: Double
+    )
+    fun loadRewards() {
+        realRewards.clear()
+        for (line in rewards){
+            val params = line.split(":")
+            val global = when (params.getOrNull(0) ?: error("不存在参数: 经济上下文")) {
+                "global" -> true
+                "group" -> false
+                else -> error("参数错误: 应为 global 或 group")
+            }
+            val currencyName = params.getOrNull(1) ?: error("不存在参数: 货币种类")
+            val currency = EconomyService.basket[currencyName] ?: error("货币种类 $currencyName 不存在")
+            val money = (params.getOrNull(2) ?: error("不存在参数: 金钱数量")).toDoubleOrNull() ?: error("错误: 填入的金钱数量应为数字")
+            realRewards.add(Reward(global, currency, money))
+        }
+    }
+    fun giveRewardsTo(group: Group, user: User) {
+        realRewards.forEach {
+            it.run {
+                if (isGlobal) globalEconomy {
+                    service.account(user) += (currency to money)
+                } else group.economy {
+                    service.account(user) += (currency to money)
+                }
+            }
+        }
+    }
 }
