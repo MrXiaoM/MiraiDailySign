1cf3ba7c335cb491eae7536d38b878c665129c12
ヒューマンワーキング <coolxiaom95@gmail.com>
2022-11-12 14:20:45 +0800

    Create replace.js

---
+++ src/main/resources/replace.js
@@ -0,0 +1,11 @@
+function replace(s, event) {
+    let d = new Date();
+    let group = event.getGroup();
+    let sender = event.getSender();
+    return s
+    .replaceAll("$namecardOrNick", sender.getNameCardOrNick())
+    .replaceAll("$id", sender.getId())
+    .replaceAll("$date", d.getFullYear() + "年" + (d.getMonth() + 1) + "月" + d.getDate() + "日")
+    .replaceAll("$week", ["星期日","星期一","星期二","星期三",,"星期四","星期五","星期六"][d.getDay()])
+    .replaceAll("$time", d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds());
+}
