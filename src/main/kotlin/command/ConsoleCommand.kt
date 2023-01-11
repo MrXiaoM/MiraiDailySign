93aaa1dee7275d19319ec74b45a902662b3b1abb
ヒューマンワーキン <coolxiaom95@gmail.com>
2023-01-11 11:56:50 +0800

    use composite command instead of simple one

--- src/main/kotlin/command/ConsoleCommand.kt
+++ src/main/kotlin/command/ConsoleCommand.kt
@@ -1,22 +1,21 @@
 package top.mrxiaom.mirai.dailysign.command
 
 import net.mamoe.mirai.console.command.CommandSender
+import net.mamoe.mirai.console.command.CompositeCommand
 import net.mamoe.mirai.console.command.ConsoleCommandSender
 import net.mamoe.mirai.console.command.SimpleCommand
 import top.mrxiaom.mirai.dailysign.MiraiDailySign
 
-object ConsoleCommand : SimpleCommand(
+object ConsoleCommand : CompositeCommand(
     owner = MiraiDailySign,
     primaryName = "MiraiDailySign",
     secondaryNames = arrayOf("dailysign", "sign"),
     parentPermission = MiraiDailySign.parentPermission
 ) {
-    @Handler
-    suspend fun CommandSender.handle(operation: String) {
-        if (operation.equals("reload", true)) {
-            MiraiDailySign.reloadConfig()
-            if (this !is ConsoleCommandSender) sendMessage("配置文件已重载")
-            MiraiDailySign.logger.info("配置文件已重载")
-        }
+    @SubCommand("reload")
+    suspend fun reload(sender: CommandSender) {
+        MiraiDailySign.reloadConfig()
+        if (sender !is ConsoleCommandSender) sender.sendMessage("配置文件已重载")
+        MiraiDailySign.logger.info("配置文件已重载")
     }
 }
