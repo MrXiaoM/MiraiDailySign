54ff66420568dbabd6045c22e60ae8db4097575a
ヒューマンワーキン <coolxiaom95@gmail.com>
2023-01-11 11:57:12 +0800

    upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,19 +3,20 @@ plugins {
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
 
-    id("net.mamoe.mirai-console") version "2.13.0"
+    id("net.mamoe.mirai-console") version "2.13.2"
 }
 
 group = "top.mrxiaom"
 version = "0.1.0"
 
 repositories {
-    maven("https://maven.aliyun.com/repository/central")
+    maven("https://repo.huaweicloud.com/repository/maven/")
     mavenCentral()
 }
 
 dependencies {
-    compileOnly("xyz.cssxsh.mirai:mirai-economy-core:1.0.1")
+    compileOnly("xyz.cssxsh.mirai:mirai-economy-core:1.0.6")
+    compileOnly("xyz.cssxsh.mirai:mirai-hibernate-plugin:2.5.1")
     implementation("org.mozilla:rhino:1.7.14")
 }
 
