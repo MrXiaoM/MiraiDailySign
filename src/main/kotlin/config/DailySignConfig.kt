e800efd3848d0501544d07c1c88b5771a86f36d2
MrXiaoM <coolxiaom95@gmail.com>
2023-09-09 18:58:06 +0800

    better ValueDescription

--- src/main/kotlin/config/DailySignConfig.kt
+++ src/main/kotlin/config/DailySignConfig.kt
@@ -25,35 +25,47 @@ class DailySignConfig(
     val fileName: String = "default"
 ) : ReadOnlyPluginConfig("groups/$fileName") {
     @ValueName("global")
-    @ValueDescription("该配置是否为全局配置，全局签到的统计数据在所有群通用，关闭此项后签到将视为在某群签到，签到天数等统计数据独立")
+    @ValueDescription("""
+        该配置是否为全局配置，全局签到的统计数据在所有群通用，关闭此项后签到将视为在某群签到，签到天数等统计数据独立
+    """)
     val global by value(true)
     @ValueName("at")
-    @ValueDescription("需要 @ 机器人才能签到")
+    @ValueDescription("""
+        需要 @ 机器人才能签到
+    """)
     val at by value(true)
     @ValueName("keywords")
-    @ValueDescription("签到命令关键词，关键词会忽略消息前后空格")
+    @ValueDescription("""
+        签到命令关键词，关键词会忽略消息前后空格
+    """)
     val keywords by value(listOf("签到"))
     @ValueName("permission")
-    @ValueDescription("签到所需权限，留空为不需要权限\n" +
-            "若设置权限，则需要 top.mrxiaom.mirai.dailysign:你输入的值\n" +
-            "这个权限才能签到，其中 \$file 会被替换成当前配置文件名(不含后缀)\n" +
-            "以默认配置文件为例，需要权限 top.mrxiaom.mirai.dailysign:sign.default")
+    @ValueDescription("""
+        签到所需权限，留空为不需要权限
+        若设置权限，则需要 top.mrxiaom.mirai.dailysign:你输入的值
+        这个权限才能签到，其中 ${"\$"}file 会被替换成当前配置文件名(不含后缀)
+        以默认配置文件为例，需要权限 top.mrxiaom.mirai.dailysign:sign.default
+    """)
     val permission by value("sign.\$file")
     fun hasPerm(permitteeId: PermitteeId) : Boolean = PermissionHolder[permission].testPermission(permitteeId)
     fun hasPerm(member: Member) : Boolean = hasPerm(AbstractPermitteeId.ExactMember(member.group.id, member.id))
 
     @ValueName("deny-message")
-    @ValueDescription("没有权限时的提示(列表，换行)，留空[]为不提示\n" +
-            "其中，\$quote 为回复消息，\$at 为 @，\$perm 为用户缺少的权限")
+    @ValueDescription("""
+        没有权限时的提示(列表，换行)，留空[]为不提示
+        其中，${"\$"}quote 为回复消息，${"\$"}at 为 @，${"\$"}perm 为用户缺少的权限
+    """)
     val denyMessage by value(listOf<String>())
     @ValueName("success-message")
-    @ValueDescription("签到成功时的提示(列表，换行)，留空[]为不提示\n" +
-            "其中，\$quote 为回复消息，\$at 为 @，\$lasting 为连续签到天数\n" +
-            "\$rewards 为签到奖励，\$avatar 为用户头像(加载失败时显示QQ号)\n" +
-            "暂时懒得写表情，PRs welcome\n" +
-            "因为想到一些签到插件还会有什么奇奇怪怪的无用提示，所以我都给加上变量了，请编辑以下脚本查看\n" +
-            "config/top.mrxiaom.mirai.dailysign/replace.js\n" +
-            "如有需要可自行添加变量(仅文字)，每当需要替换变量的时候都会执行一次该脚本")
+    @ValueDescription("""
+        签到成功时的提示(列表，换行)，留空[]为不提示
+        其中，${"\$"}quote 为回复消息，${"\$"}at 为 @，${"\$"}lasting 为连续签到天数
+        ${"\$"}rewards 为签到奖励，${"\$"}avatar 为用户头像(加载失败时显示QQ号)
+        暂时懒得写表情，PRs welcome
+        因为想到一些签到插件还会有什么奇奇怪怪的无用提示，所以我都给加上变量了，请编辑以下脚本查看
+        config/top.mrxiaom.mirai.dailysign/replace.js
+        如有需要可自行添加变量(仅文字)，每当需要替换变量的时候都会执行一次该脚本
+    """)
     val successMessage by value(listOf(
         "\$quote",
         "\$avatar",
@@ -65,18 +77,24 @@ class DailySignConfig(
         "祝你有美好的一天"
     ))
     @ValueName("already-sign-message")
-    @ValueDescription("今日已签到的提示(列表，换行)，留空[]为不提示\n" +
-            "其中，\$quote 为回复消息，\$at 为 @，\$avatar 为用户头像(加载失败时显示QQ号)\n" +
-            "变量同上")
+    @ValueDescription("""
+        今日已签到的提示(列表，换行)，留空[]为不提示
+        其中，${"\$"}quote 为回复消息，${"\$"}at 为 @，${"\$"}avatar 为用户头像(加载失败时显示QQ号)
+        变量同上
+    """)
     val alreadySignMessage by value(listOf(
         "\$quote你今天已经签到过了"
     ))
 
     @ValueName("reward-template-global")
-    @ValueDescription("签到奖励模板(全局上下文)，其中 \$currency 为货币种类，\$money 为货币数量")
+    @ValueDescription("""
+        签到奖励模板(全局上下文)，其中 ${"\$"}currency 为货币种类，${"\$"}money 为货币数量
+    """)
     val rewardTemplateGlobal by value("★ \$currency * \$money\n")
     @ValueName("reward-template-group")
-    @ValueDescription("签到奖励模板(群聊上下文)，其中 \$currency 为货币种类，\$money 为货币数量")
+    @ValueDescription("""
+        签到奖励模板(群聊上下文)，其中 ${"\$"}currency 为货币种类，${"\$"}money 为货币数量
+    """)
     val rewardTemplateGroup by value("☆ \$currency * \$money\n")
 
     fun getRewardTemple(info: RewardInfo): String =
@@ -85,11 +103,13 @@ class DailySignConfig(
         .replace("\$money", info.money.toString())
 
     @ValueName("rewards")
-    @ValueDescription("签到奖励，格式如下\n" +
-            "奖励金钱到群聊上下文 group:货币种类:数量\n" +
-            "奖励金钱到全局上下文 global:货币种类:数量\n" +
-            "数量可填固定数量，如 50，也可以用-符号连接两个整数来表示随机数，如 50-100\n" +
-            "随机数的上界和下界均可取得")
+    @ValueDescription("""
+        签到奖励，格式如下
+        奖励金钱到群聊上下文 group:货币种类:数量
+        奖励金钱到全局上下文 global:货币种类:数量
+        数量可填固定数量，如 50，也可以用-符号连接两个整数来表示随机数，如 50-100
+        随机数的上界和下界均可取得
+    """)
     val rewards by value(listOf("group:mirai-coin:100"))
     private val realRewards = mutableListOf<Reward>()
 
